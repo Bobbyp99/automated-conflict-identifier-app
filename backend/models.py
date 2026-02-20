fd5bb7597b41849b1617cc8150c23690b0f7f197
bobbyp99 <bobbyp470@gmail.com>
2026-02-20 08:55:34 -0800

    setup progress

---
+++ backend/models.py
@@ -0,0 +1,8 @@
+from sqlalchemy import Column, Integer, String
+from database import Base
+
+class Official(Base):
+    __tablename__ = "officials"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
