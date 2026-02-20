e78905f06f1e06449d8e6a3d4885076d1b8830ce
Andi Tavares <anditavares64@gmail.com>
2026-02-20 14:57:43 -0800

    testing again

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,8 @@ from fastapi import FastAPI
 app = FastAPI()
 
 # From Neil! In branch test v2, no less!
+# From Andi! Testing to see if git is working in test3 now!
+# From Andi again! And again from my Windows PC!
 @app.get("/")
 def read_root():
     return {"status": "API is running"}
