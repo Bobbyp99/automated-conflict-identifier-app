3952204991a9c8831d8ef13c4479e837e6bfcc2f
bobbyp99 <bobbyp470@gmail.com>
2026-02-23 17:42:40 -0800

    add

--- backend/models.py
+++ backend/models.py
@@ -1,8 +1,28 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
 from backend.database import Base
 
-class Official(Base):
+class Officials(Base):
     __tablename__ = "officials"
 
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, nullable=False)
+    #id = Column(Integer, primary_key=True, index=True)
+    #first_name = Column(String)
+    id: Mapped[int] = mapped_column(primary_key = True) # do once for each table
+
+    first_name: Mapped[str] = mapped_column(String)
+    last_name: Mapped[str] = mapped_column(String)
+    middle_name: Mapped[str] = mapped_column(String)
+
+
+
+class Jurisdictions(Base):
+    __tablename__ = "jurisdictions"
+
+    id: Mapped[int] = mapped_column(primary_key = True) 
+
+    jurisdiction_name: Mapped[str] = mapped_column(String)
+
+
+
+
+
