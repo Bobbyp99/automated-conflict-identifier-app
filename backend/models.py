80be2aab7c353d157dcc5212d244c0b103d8221d
bobbyp99 <bobbyp470@gmail.com>
2026-02-25 17:43:26 -0800

    Progress on database construction

--- backend/models.py
+++ backend/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import String, ForeignKey
+from sqlalchemy import String, Float, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from typing import List
 from backend.database import Base
@@ -18,10 +18,8 @@ class Officials(Base):
 
     cleaned_name: Mapped[str] = mapped_column(String)  # this will be all lowercase for easier fuzzy search implementation
 
-    #Relationships
+    # Relationships
     jurisdiction: Mapped["Jurisdictions"] = relationship(back_populates = "officials")
-
-
 
 
 class Jurisdictions(Base):
@@ -32,15 +30,49 @@ class Jurisdictions(Base):
 
     jurisdiction_name: Mapped[str] = mapped_column(String)
 
-    #Relationships
+    # Relationships
     officials: Mapped[List["Officials"]] = relationship(back_populates = "jurisdictions")
 
 
-#class Flags(Base):
-  #  __tablename__ = "flags"
+class Decisions(Base):
+    __tablename__ = "decisions"
 
-   # id: Mapped[int] = mapped_column(primary_key = True)
+    id: Mapped[int] = mapped_column(primary_key = True)
+    jurisdiction_id: Mapped[int] = mapped_column(ForeignKey("jurisdictions.id"))
 
+    decision_date: Mapped[str] = mapped_column(String)
+    title: Mapped[str] = mapped_column(String)
+    decision_url: Mapped[str] = mapped_column(String)  # client mentioned we might want to include the URL in output 
+
+    # Relationships
+
+
+class DecisionParticipants(Base):
+     __tablename__ = "decision_participants"
+
+     id: Mapped[int] = mapped_column(primary_key = True)
+
+     name_raw: Mapped[str] = mapped_column(String)
+
+
+
+# This table will actually estimate whether or not a participant is an official 
+# dedicated folder of python files for this, but only after ingestion of data into database
+class ParticipantMatch(Base):
+    __tablename__ = "participant_matches"
+	 
+    id: Mapped[int] = mapped_column(primary_key = True)
+    match_score: Mapped[float] = mapped_column(Float)
+
+
+class ConflictFlag(Base):
+    __tablename__ = "conflict_flags"
+
+    id: Mapped[int] = mapped_column(primary_key = True)
+    officials_id: Mapped[int] = mapped_column(ForeignKey("officials.id"))
+
+    # we will store the explanation of the match (x official, y decision on date, z source of income year))
+    explanation: Mapped[str] = mapped_column(String)
 
     
 
