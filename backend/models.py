0bf32326fcaa3a9414e8993d000508a663d2cc45
bobbyp99 <bobbyp470@gmail.com>
2026-02-25 16:08:44 -0800

    relationship between Jurisdictions and Officials

--- backend/models.py
+++ backend/models.py
@@ -1,5 +1,6 @@
-from sqlalchemy import String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from typing import List
 from backend.database import Base
 
 class Officials(Base):
@@ -8,10 +9,18 @@ class Officials(Base):
     #id = Column(Integer, primary_key=True, index=True)
     #first_name = Column(String)
     id: Mapped[int] = mapped_column(primary_key = True) # do once for each table
+    jurisdiction_id: Mapped[int] = mapped_column(ForeignKey("jurisdictions.id"))
 
     first_name: Mapped[str] = mapped_column(String)
     last_name: Mapped[str] = mapped_column(String)
     middle_name: Mapped[str] = mapped_column(String)
+    name_suffix: Mapped[str] = mapped_column(String)
+
+    cleaned_name: Mapped[str] = mapped_column(String)  # this will be all lowercase for easier fuzzy search implementation
+
+    #Relationships
+    jurisdiction: Mapped["Jurisdictions"] = relationship(back_populates = "officials")
+
 
 
 
@@ -20,7 +29,20 @@ class Jurisdictions(Base):
 
     id: Mapped[int] = mapped_column(primary_key = True) 
 
+
     jurisdiction_name: Mapped[str] = mapped_column(String)
+
+    #Relationships
+    officials: Mapped[List["Officials"]] = relationship(back_populates = "jurisdictions")
+
+
+#class Flags(Base):
+  #  __tablename__ = "flags"
+
+   # id: Mapped[int] = mapped_column(primary_key = True)
+
+
+    
 
 
 
