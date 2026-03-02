8e1ebe0a4a7b834a4ebb2fc989488c1cadd2046e
Enrique Cabrera <enriquecabrera32123@gmail.com>
2026-03-02 11:25:30 -0800

    ingest jurisdiction and officials

--- backend/ingest/ingest_data.py
+++ backend/ingest/ingest_data.py
@@ -12,55 +12,47 @@ from sqlalchemy import text
 sys.path.append(str(Path(__file__).parents[1]))
 from database import engine
 
-# gets information about each official
-def get_official_data(sheet_data):
-    if "First Name" not in sheet_data.columns or "Last Name" not in sheet_data.columns or "Middle Name" not in sheet_data.columns:
-        return
-    new_df = pd.DataFrame()
-    new_df['first_name'] = sheet_data[["First Name"]]
-    new_df['last_name'] = sheet_data[["Last Name"]]
-    new_df["middle_name"] = sheet_data["Middle Name"].fillna("")
-    new_df = new_df.dropna(subset=['first_name', 'last_name'])
-    new_df["cleaned_name"] = new_df["first_name"].str.lower() + " " + new_df["middle_name"].str.lower() + " " + new_df["last_name"].str.lower()
-    new_df["name_suffix"] = ""
-    new_df["jurisdiction_id"] = 1
 
-    return new_df
-
-def get_jurisdiction_data(sheet_data):
-    if "Agency" not in sheet_data.columns:
-        return
-    new_df = pd.DataFrame()
-    new_df["jurisdiction_name"] = sheet_data[["Agency"]]
-    new_df = new_df.dropna(subset=['jurisdiction_name'])
-    new_df = new_df.drop_duplicates(subset=['jurisdiction_name'])
-
-    return new_df
-
-
-
-def ingest_data(sheets):
+def ingest_official_data(sheets):
     all_jurisdictions = []
-    all_officials = []
-    # NEED TO ADD A WAY TO ADD JURISDICTIONS FIRST, SO THAT WE CAN USE THAT ID TO LINK OFFICIALS TO JURISDICTIONS, CURRENTLY JUST SETTING ALL JURISDICTION IDS TO 1 FOR TESTING PURPOSES
     for sheet_name, sheet_data in sheets.items():
-        jurisdictions_df = get_jurisdiction_data(sheet_data)
-        # officials_df = get_official_data(sheet_data)
-
-        if jurisdictions_df is not None:
-                all_jurisdictions.append(jurisdictions_df)
-        # if officials_df is not None:
-        #     all_officials.append(officials_df)
+        if "First Name" not in sheet_data.columns or "Last Name" not in sheet_data.columns or "Middle Name" not in sheet_data.columns:
+            continue
+        new_df = pd.DataFrame()
+        new_df['first_name'] = sheet_data[["First Name"]]
+        new_df['last_name'] = sheet_data[["Last Name"]]
+        new_df["middle_name"] = sheet_data["Middle Name"].fillna("")
+        new_df = new_df.dropna(subset=['first_name', 'last_name'])
+        new_df["cleaned_name"] = new_df["first_name"].str.lower() + " " + new_df["middle_name"].str.lower() + " " + new_df["last_name"].str.lower()
+        new_df["name_suffix"] = ""
+        new_df["jurisdiction_id"] = 1 # this is just a placeholder, will need to be updated to reflect actual jurisdiction id from jurisdictions table
+        if new_df is not None:
+                all_jurisdictions.append(new_df)
     
     all_jurisdictions_df = pd.concat(all_jurisdictions, ignore_index=True)
-    all_jurisdictions_df = all_jurisdictions_df.drop_duplicates(subset=['jurisdiction_name'])
+    all_jurisdictions_df = all_jurisdictions_df.drop_duplicates(subset=['cleaned_name'])
+    
+    save_to_postgres(all_jurisdictions_df, "officials")
 
-    # all_officials_df = pd.concat(all_officials, ignore_index=True)
-    # all_officials_df = all_officials_df.drop_duplicates(subset=['cleaned_name'])
 
-    save_to_postgres(all_jurisdictions_df, "jurisdictions")
-    # save_to_postgres(all_officials_df, "officials")
+def ingest_jurisdiction_data(sheets):
+    all_jurisdictions = []
+    for sheet_name, sheet_data in sheets.items():
+        if "Agency" not in sheet_data.columns:
+            continue
+        new_df = pd.DataFrame()
+        new_df["jurisdiction_name"] = sheet_data[["Agency"]]
+        new_df = new_df.dropna(subset=['jurisdiction_name'])
+        new_df = new_df.drop_duplicates(subset=['jurisdiction_name'])
+
+        if new_df is not None:
+                all_jurisdictions.append(new_df)
         
+    all_jurisdictions_df = pd.concat(all_jurisdictions, ignore_index=True)
+    all_jurisdictions_df = all_jurisdictions_df.drop_duplicates(subset=['jurisdiction_name'])
+    
+    save_to_postgres(all_jurisdictions_df, "jurisdictions")
+
 
 def clean_sheets(sheets):
     cleaned_sheets = {}
@@ -69,8 +61,6 @@ def clean_sheets(sheets):
 
         # modify all sheet data here, remove NaN values, etc.
         
-
-
         cleaned_sheets[sheet_name] = sheet_data
     return cleaned_sheets
 
@@ -101,7 +91,9 @@ def save_to_postgres(df, table_name):
         df.to_sql(table_name, con=connection, if_exists='append', index=False)
 
 
-
+def ingest_data(sheets):    
+    ingest_jurisdiction_data(sheets)
+    ingest_official_data(sheets)
 
 
 if __name__ == "__main__":
