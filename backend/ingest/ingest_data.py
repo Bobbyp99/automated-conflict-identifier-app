441e8973edf5a599322fc7421e9809811b5deb16
Enrique Cabrera <enriquecabrera32123@gmail.com>
2026-03-02 14:11:49 -0800

    officials and jurisdictions

--- backend/ingest/ingest_data.py
+++ backend/ingest/ingest_data.py
@@ -14,10 +14,20 @@ from database import engine
 
 
 def ingest_official_data(sheets):
-    all_jurisdictions = []
+    all_officials = []
+
+    jurisdiction_df = pd.read_sql(text("SELECT id, jurisdiction_name FROM jurisdictions"), con=engine)
     for sheet_name, sheet_data in sheets.items():
         if "First Name" not in sheet_data.columns or "Last Name" not in sheet_data.columns or "Middle Name" not in sheet_data.columns:
             continue
+        
+        sheet_data = sheet_data.merge(
+        jurisdiction_df[['id', 'jurisdiction_name']], 
+        left_on="Agency", 
+        right_on="jurisdiction_name", 
+        how="left"
+        )
+
         new_df = pd.DataFrame()
         new_df['first_name'] = sheet_data[["First Name"]]
         new_df['last_name'] = sheet_data[["Last Name"]]
@@ -25,14 +35,20 @@ def ingest_official_data(sheets):
         new_df = new_df.dropna(subset=['first_name', 'last_name'])
         new_df["cleaned_name"] = new_df["first_name"].str.lower() + " " + new_df["middle_name"].str.lower() + " " + new_df["last_name"].str.lower()
         new_df["name_suffix"] = ""
-        new_df["jurisdiction_id"] = 1 # this is just a placeholder, will need to be updated to reflect actual jurisdiction id from jurisdictions table
+        new_df["jurisdiction_id"] = sheet_data["id"]
+
+
         if new_df is not None:
-                all_jurisdictions.append(new_df)
+                all_officials.append(new_df)
     
-    all_jurisdictions_df = pd.concat(all_jurisdictions, ignore_index=True)
-    all_jurisdictions_df = all_jurisdictions_df.drop_duplicates(subset=['cleaned_name'])
-    
-    save_to_postgres(all_jurisdictions_df, "officials")
+    all_officials_df = pd.concat(all_officials, ignore_index=True)
+    all_officials_df = all_officials_df.drop_duplicates(subset=['cleaned_name'])
+
+    with engine.begin() as connection:
+        existing_officials = pd.read_sql(text("SELECT cleaned_name FROM officials"), con=connection)
+        existing_cleaned_names = set(existing_officials['cleaned_name'])
+        new_officials_df = all_officials_df[~all_officials_df['cleaned_name'].isin(existing_cleaned_names)]
+        new_officials_df.to_sql("officials", con=connection, if_exists="append", index=False)
 
 
 def ingest_jurisdiction_data(sheets):
@@ -41,7 +57,7 @@ def ingest_jurisdiction_data(sheets):
         if "Agency" not in sheet_data.columns:
             continue
         new_df = pd.DataFrame()
-        new_df["jurisdiction_name"] = sheet_data[["Agency"]]
+        new_df["jurisdiction_name"] = sheet_data["Agency"]
         new_df = new_df.dropna(subset=['jurisdiction_name'])
         new_df = new_df.drop_duplicates(subset=['jurisdiction_name'])
 
@@ -49,9 +65,13 @@ def ingest_jurisdiction_data(sheets):
                 all_jurisdictions.append(new_df)
         
     all_jurisdictions_df = pd.concat(all_jurisdictions, ignore_index=True)
-    all_jurisdictions_df = all_jurisdictions_df.drop_duplicates(subset=['jurisdiction_name'])
+    all_jurisdictions_df = all_jurisdictions_df.drop_duplicates(subset=['jurisdiction_name']) 
     
-    save_to_postgres(all_jurisdictions_df, "jurisdictions")
+    with engine.begin() as connection:
+        existing_jurisdictions = pd.read_sql(text("SELECT jurisdiction_name FROM jurisdictions"), con=connection)
+        existing_jurisdiction_names = set(existing_jurisdictions['jurisdiction_name'])
+        new_jurisdictions_df = all_jurisdictions_df[~all_jurisdictions_df['jurisdiction_name'].isin(existing_jurisdiction_names)]
+        new_jurisdictions_df.to_sql("jurisdictions", con=connection, if_exists="append", index=False)
 
 
 def clean_sheets(sheets):
@@ -64,14 +84,12 @@ def clean_sheets(sheets):
         cleaned_sheets[sheet_name] = sheet_data
     return cleaned_sheets
 
-
-# currently reads excel files, and save each sheet as a csv file in the processed data directory
+# loads excel files from data/raw, returns a dictionary of sheet name to sheet data (as a pandas dataframe)
 def get_sheets(file_path):
 
     excel_file_paths = list((file_path / "raw").glob("*.xlsx"))
 
     clean_sheets = {}
-
 
     for excel_file_path in excel_file_paths:
 
@@ -86,10 +104,6 @@ def get_sheets(file_path):
     return clean_sheets
 
 
-def save_to_postgres(df, table_name):
-    with engine.connect() as connection:
-        df.to_sql(table_name, con=connection, if_exists='append', index=False)
-
 
 def ingest_data(sheets):    
     ingest_jurisdiction_data(sheets)
@@ -103,7 +117,3 @@ if __name__ == "__main__":
     sheets = get_sheets(file_path)
     sheets = clean_sheets(sheets)
     ingest_data(sheets)
-
-
-# current problems
-# Currently takes in duplicates of officials and jurisdictions when taking in new data.
