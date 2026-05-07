efbd99b541ec70cc5f09a79fd247a3aa0587978b
anditavares23 <anditavares64@gmail.com>
2026-05-07 03:31:23 -0700

    Delete backend directory

--- backend/models.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import Column, Integer, String
-from backend.database import Base
-
-class Official(Base):
-    __tablename__ = "officials"
-
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, nullable=False)
